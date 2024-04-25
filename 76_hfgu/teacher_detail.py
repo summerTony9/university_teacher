fa9dff70d7ddb63498f0c483478b7a36918da788
luyueti111 <201811011123@mail.bnu.edu.cn>
2024-04-25 21:48:39 +0800

    DONE

--- 76_hfgu/teacher_detail.py
+++ 76_hfgu/teacher_detail.py
@@ -13,39 +13,65 @@ print(df.head())
 result_list = []
 error_df = []
 
-for index in range(104, 105):
+for index in range(len(df)):
     time.sleep(random.uniform(3, 5))
     teacher_name = df.loc[index, '姓名']
     teacher_url = df.loc[index, 'url']
     print(teacher_name, index)
     teacher_dict = {'姓名': teacher_name, 'url': teacher_url}
 
-    # try:
-    html = requests.get(teacher_url, headers={'User-Agent': UserAgent().random}).content
-    bs0bj = BeautifulSoup(html, features='html.parser')
-    table = bs0bj.find('center').find('tbody')
-    rows = table.find_all('tr')
+    try:
+        html = requests.get(teacher_url, headers={'User-Agent': UserAgent().random}).content
+        bs0bj = BeautifulSoup(html, features='html.parser')
+        table = bs0bj.find('tbody')
+        rows = table.find_all('tr')
 
-    for row in rows:
-        # print(row)
-        row_info_list = row.find_all('li')
-        if len(row_info_list) == 0:
-            key = row.find('td').get_text()
-            value = row.find('td').find_next('td').get_text()
-            teacher_dict[key] = value.replace('\xa0', '').replace('\r', "")
-
-        else:
-            for info in row_info_list:
-                print(info)
-                key = info.find('div').get_text()
-                value = info.find('div').find_next('div').get_text()
+        for row in rows:
+            # print(row)
+            cls = row.get('class')
+            # row_info_list = row.find_all('li')
+            if cls is None:
+                try:
+                    key = row.find('p').get_text()
+                    value = row.find_all('p')[1:]
+                    value = [v.get_text() for v in value]
+                    value = ''.join(value)
+                except Exception as e:
+                    key = row.find('td').get_text()
+                    value = row.find_all('td')[1:]
+                    value = [v.get_text() for v in value]
+                    value = ''.join(value)
+                key = key.replace('\xa0', '').replace('\r', "").replace(' ', '')
                 teacher_dict[key] = value.replace('\xa0', '').replace('\r', "")
-    result_list.append(teacher_dict)
-    # except Exception as e:
-    #     print(e)
-    #     print(index, teacher_name, teacher_url)
-    #     error_df.append({'teacher_name': teacher_name, 'teacher_url': teacher_url})
-    #     continue
+
+            else:
+                row_info_list = row.find_all('li')
+                value = ""
+                key = ""
+                for info in row_info_list:
+                    # print(info)
+                    # print(info.find('div').get('class'))
+                    if info.find('div').get('class') is not None:
+                        if info.find('div').get('class')[0] == 'ez_hd':
+                            if key != "":
+                                key = key.replace('\xa0', '').replace('\r', "").replace(' ', '')
+                                teacher_dict[key] = value.replace('\xa0', '').replace('\r', "")
+                            key = info.find('div').get_text()
+                            value = info.find_all('div')[1:]
+                            value = [v.get_text() for v in value]
+                            value = ''.join(value)
+                    else:
+                        # key = info.find('div').get_text()
+                        print(info.get_text())
+                        if info.get_text() is not None:
+                            value += info.get_text()
+                # teacher_dict[key] = value.replace('\xa0', '').replace('\r', "")
+        result_list.append(teacher_dict)
+    except Exception as e:
+        print(e)
+        print(index, teacher_name, teacher_url)
+        error_df.append({'teacher_name': teacher_name, 'teacher_url': teacher_url})
+        continue
 
 
 def merge_dicts_with_list_values(dict_list):
@@ -62,7 +88,7 @@ def merge_dicts_with_list_values(dict_list):
 
     return merged_dict
 
-exit()
+
 result_df = pd.DataFrame(
     merge_dicts_with_list_values(result_list)
 )
