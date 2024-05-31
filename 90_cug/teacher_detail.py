15c42ad7d92fee4d98f445011200b87bb9d9a8bc
luyueti111 <201811011123@mail.bnu.edu.cn>
2024-05-31 16:20:16 +0800

    tidy result

--- 90_cug/teacher_detail.py
+++ 90_cug/teacher_detail.py
@@ -5,7 +5,6 @@ import requests
 from fake_useragent import UserAgent
 import pandas as pd
 
-
 def extract_teacher_info(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
 
@@ -38,67 +37,39 @@ def extract_teacher_info(html_content):
                 if len(key_value) == 2:
                     teacher_info[key_value[0].strip()] = key_value[1].strip()
 
-    # Check for alternative basic info layout
-    alternative_basic_info = soup.find_all('li')
-    for li in alternative_basic_info:
-        text = li.get_text(strip=True)
-        if '性别' in text:
-            teacher_info['性别'] = text.split(':')[-1].strip()
-        elif '出生年月' in text:
-            teacher_info['出生年月'] = text.split(':')[-1].strip()
-        elif '毕业院校' in text:
-            teacher_info['毕业院校'] = text.split(':')[-1].strip()
-        elif '学历' in text:
-            teacher_info['学历'] = text.split(':')[-1].strip()
-        elif '学位' in text:
-            teacher_info['学位'] = text.split(':')[-1].strip()
-        elif '所在单位' in text:
-            teacher_info['所在单位'] = text.split(':')[-1].strip()
-        elif '办公地点' in text:
-            teacher_info['办公地点'] = text.split(':')[-1].strip()
-        elif 'Email' in text:
-            teacher_info['Email'] = text.split(':')[-1].strip()
-
     # Extracting personal profile
-    profile_section = soup.find('div', class_='t_grjj_nr') or soup.find('div', class_='CurrencyBox', text='个人简介')
+    profile_section = soup.find('div', class_='t_grjj_nr')
     if profile_section:
         teacher_info['个人简介'] = profile_section.get_text(strip=True)
 
     # Extracting education background
-    education_section = soup.find('div', class_='bd') or soup.find('div', class_='CurrencyBox', text='教育经历')
+    education_section = soup.find('div', class_='bd')
     education_list = []
     if education_section:
-        education_items = education_section.find_all('ul')
+        education_items = education_section.find_all('ul', class_='jyjlulu13')
         for item in education_items:
             education = {}
-            dates = item.find_previous('div', class_='t_e_date').get_text(strip=True) if item.find_previous('div',
-                                                                                                            class_='t_e_date') else None
+            dates = item.find_previous('div', class_='t_e_date').get_text(strip=True)
             education['时间'] = dates
-            li_elements = item.find_all('li')
-            if len(li_elements) >= 4:
-                education['学校'] = li_elements[0].get_text(strip=True)
-                education['专业'] = li_elements[1].get_text(strip=True)
-                education['学历'] = li_elements[2].get_text(strip=True)
-                education['学位'] = li_elements[3].get_text(strip=True)
-            else:
-                education['内容'] = li_elements[0].get_text(strip=True)
+            education['学校'] = item.find_all('li')[0].get_text(strip=True)
+            education['专业'] = item.find_all('li')[1].get_text(strip=True)
+            education['学历'] = item.find_all('li')[2].get_text(strip=True)
+            education['学位'] = item.find_all('li')[3].get_text(strip=True)
             education_list.append(education)
         teacher_info['教育经历'] = education_list
 
     # Extracting work experience
-    work_experience_section = soup.find('div', class_='CurrencyBox', text='工作经历')
+    work_experience_section = soup.find('ul', class_='gzjlu14')
     work_experience_list = []
     if work_experience_section:
         work_experience_items = work_experience_section.find_all('li')
-        for item in work_experience_items:
-            work_experience = {}
-            dates = item.find('p').get_text(strip=True) if item.find('p') else None
-            work_experience['时间'] = dates
-            li_elements = item.find_all('li')
-            if len(li_elements) >= 3:
-                work_experience['单位'] = li_elements[0].get_text(strip=True)
-                work_experience['职位'] = li_elements[1].get_text(strip=True)
-            work_experience_list.append(work_experience)
+        work_dates = work_experience_section.find_previous('div', class_='t_e_date').get_text(strip=True)
+        work_experience = {
+            '时间': work_dates,
+            '单位': work_experience_items[0].get_text(strip=True),
+            '职位': work_experience_items[1].get_text(strip=True),
+        }
+        work_experience_list.append(work_experience)
         teacher_info['工作经历'] = work_experience_list
 
     return teacher_info
@@ -137,7 +108,6 @@ for index in range(len(df)):
     teacher_info['系'] = dept
     teacher_info['职称'] = title
     teacher_info['链接'] = teacher_url
-    print(teacher_info)
 
     dict_list.append(teacher_info)
 
